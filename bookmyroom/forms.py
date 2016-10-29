5093f1003e6e7bbe49df74f69a7e883ac7d2993e
HitenSethiya <hitensethiya@hotmail.com>
2016-10-29 11:52:44 +0530

    done everything except date paginator js function

--- bookmyroom/forms.py
+++ bookmyroom/forms.py
@@ -9,6 +9,20 @@ class BookForm(forms.ModelForm):
         fields = ['room_name','date', 'in_time', 'out_time']
         widgets = {
             'date': forms.DateInput(attrs={'id':'datepicker'}),
-            'in_time': forms.TimeInput(attrs={'id': 'timepicker'}),
-            'out_time': forms.TimeInput(attrs={'id': 'timepicker'}),
         }
+    def validate_form(self):
+        name= self.cleaned_data.get('room_name')
+        date= self.cleaned_data.get('date')
+        in_time= self.cleaned_data.get('in_time')
+        out_time= self.cleaned_data.get('out_time')
+        room = Room_Booking.objects.filter(date=date).filter(room_name=name).order_by('in_time').filter(in_time__lte=out_time,out_time__gte=in_time).exists()
+        if room:
+            raise forms.ValidationError("Room not Available in this time period check your dashboard")
+        else:
+            pass
+
+
+
+
+
+
--- bookmyroom/models.py
+++ bookmyroom/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from datetime import *
 
 
 class User(models.Model):
@@ -30,11 +31,62 @@ class Room(models.Model):
 
 
 class Room_Booking(models.Model):
+    TimeChoices = [
+        (time(0,00), u'12:00 am'),
+        (time(0,30), u'12:30 am'),
+        (time(1,00), u'1:00 am'),
+        (time(1,30), u'1:30 am'),
+        (time(2,00), u'2:00 am'),
+        (time(2,30), u'2:30 am'),
+        (time(3,00), u'3:00 am'),
+        (time(3,30), u'3:30 am'),
+        (time(4,00), u'4:00 am'),
+        (time(4,30), u'4:30 am'),
+        (time(5,00), u'5:00 am'),
+        (time(5,30), u'5:30 am'),
+        (time(6,00), u'6:00 am'),
+        (time(6,30), u'6:30 am'),
+        (time(7,00), u'7:00 am'),
+        (time(7,30), u'7:30 am'),
+        (time(8,00), u'8:00 am'),
+        (time(8,30), u'8:30 am'),
+        (time(9,00), u'9:00 am'),
+        (time(9,30), u'9:30 am'),
+        (time(10,00), u'10:00 am'),
+        (time(10,30), u'10:30 am'),
+        (time(11,00), u'11:00 am'),
+        (time(11,30), u'11:30 am'),
+        (time(12,00), u'12:00 pm'),
+        (time(12,30), u'12:30 pm'),
+        (time(13,00), u'1:00 pm'),
+        (time(13,30), u'1:30 pm'),
+        (time(14,00), u'2:00 pm'),
+        (time(14,30), u'2:30 pm'),
+        (time(15,00), u'3:00 pm'),
+        (time(15,30), u'3:30 pm'),
+        (time(16,00), u'4:00 pm'),
+        (time(16,30), u'4:30 pm'),
+        (time(17,00), u'5:00 pm'),
+        (time(17,30) ,u'5:30 pm'),
+        (time(18,00), u'6:00 pm'),
+        (time(18,30), u'6:30 pm'),
+        (time(19,00), u'7:00 pm'),
+        (time(19,30), u'7:30 pm'),
+        (time(20,00), u'8:00 pm'),
+        (time(20,30), u'8:30 pm'),
+        (time(21,00), u'9:00 pm'),
+        (time(21,30), u'9:30 pm'),
+        (time(22,00), u'10:00 pm'),
+        (time(22,30), u'10:30 pm'),
+        (time(23,00), u'11:00 pm'),
+        (time(23,30), u'11:30 pm'),
+
+    ]
     room_name = models.ForeignKey(Room)
     room_status = models.BooleanField(default=False)
     date = models.DateField(blank=True, null=True)
-    in_time = models.TimeField(blank=True, null=True)
-    out_time = models.TimeField(blank=True, null=True)
+    in_time = models.TimeField(blank=True, choices=TimeChoices, default='17:30')
+    out_time = models.TimeField(blank=True, choices=TimeChoices, default='17:30')
     book_time = models.DateTimeField(default=timezone.now)
     user = models.ForeignKey(User)
 
