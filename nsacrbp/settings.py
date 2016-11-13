0a5909b918d991f5fea095a167406a743019883d
Vedant Rathore <vedantr1998@gmail.com>
2016-11-13 07:15:50 +0530

    fi

--- nsacrbp/settings.py
+++ nsacrbp/settings.py
@@ -127,12 +127,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-# EMAIL_USE_TLS = True
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_HOST_USER = 'vedantr1998@gmail.com'
-# EMAIL_HOST_PASSWORD = 'abcABC123!'
-# EMAIL_PORT = 587
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
